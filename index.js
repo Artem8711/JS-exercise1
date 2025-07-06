f89a63e2d0f2fe920598c30e81cbc3a288cf4938
Artem8711 <goglil2063@gmail.com>
2025-07-06 15:07:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/* практичиское задание 1 */
+
+//1
+const a = 2;
+const b = 3;
+const multiply = a * b;
+console.log(`множення`, multiply);
+
+//2
+const c = 10;
+const d = 2;
+const divide = c / d;
+console.log(`ділення`, divide);
+
+//3
+const e = 5;
+const f = 2;
+const sum = e + f;
+console.log(`додавання`, sum);
+
+//4
+let num1 = 11;
+let bool = true;
+let str1 = 'java script';
+let str2 = '100';
+console.log(num1, bool, str1, str2);
+
+//5
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+console.log(num);
