07461ab0ec1c7dce9bb9994a0c5dc809f77381c5
Artem8711 <goglil2063@gmail.com>
2025-07-06 15:38:40 +0200

    Обновить index.html и index.js: удалить атрибут defer у скрипта и добавить новую функциональность в JS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 /* практичиское задание 1 */
 
+/* Первая часть */
+
 //1
 const a = 2;
 const b = 3;
@@ -31,4 +33,47 @@ num += 11;
 num -= 11;
 num *= 11;
 num /= 11;
+num++;
+num--;
 console.log(num);
+
+/* Вторая часть */
+
+//1
+let number = prompt('Введи число:');
+alert('Квадрат: ' + number * number);
+
+//2
+let n1 = +prompt('Перше число:');
+let n2 = +prompt('Друге число:');
+alert('Середнє: ' + (n1 + n2) / 2);
+
+//3
+let minutes = prompt('Скільки хвилин?');
+alert('Секунд: ' + minutes * 60);
+
+//4
+let greeting = 'Hello, ';
+let userName = prompt('Як тебе звати?');
+let message = greeting + userName + '!';
+console.log(message);
+document.write(`<p>${message}</p>`);
+
+//5
+let name = prompt('Ім’я:') || 'Artem';
+let surname = prompt('Прізвище:') || 'S';
+let email = prompt('Email:') || 'goglil2063@gmail.com';
+let phone = prompt('Телефон:') || '+380636363633';
+let age = prompt('Вік:') || '18';
+let img =
+   prompt('URL до зображення:') ||
+   document.write(`
+  <div style=" padding: 1rem; border: 1px solid #ccc; width: 300px;">
+    <img src="${'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'}"  style="width: 100px; border-radius: 50%;" />
+    <p><strong>Ім’я:</strong> ${name}</p>
+    <p><strong>Прізвище:</strong> ${surname}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Телефон:</strong> ${phone}</p>
+    <p><strong>Вік:</strong> ${age}</p>
+  </div>
+`);
